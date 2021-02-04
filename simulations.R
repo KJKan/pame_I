069dff4916f2371d37a8b06959612a6644bb87d7
Kees-Jan Kan <k.j.kan@uva.nl>
2021-02-04 11:11:44 +0100

    Create simulations.R

---
+++ simulations.R
@@ -0,0 +1,33 @@
+# ---------------- Data Simulation
+
+# Clear working memory
+rm(list = ls())
+
+# Load require packages
+library( "psychonetrics" )
+library( "dplyr" )
+
+# Load the Hugarian WAIS-IV model implied matrices that will be used to generate data with
+#  1. Higher order factor model ( 'g' model )
+#  2. Bifactor model
+#  2. Network model (Kan et al., 2020)
+load( 'st_sigmas.Rdata' )
+
+# Setup
+set.seed( 03012021 ) # = start internship Tasos
+nrep <- 1000         # number of replications per condition
+n    <- 1112         # sample size used ( Hungarian WAIS-IV sample size )
+
+# Simulate data  
+simdata <- lapply( st_sigmas, 
+                   function(i)
+                     { 
+                      replicate( nrep, 
+                                 mvrnorm( n, rep( 0, ncol( i ) ), i ),
+                                 simplify = FALSE ) 
+                     }
+                   )
+      
+
+
+

c66bd52fa21a4c9562a0bb35dcd1f78a5540a25c
Kees-Jan Kan <k.j.kan@uva.nl>
2021-02-08 12:40:10 +0100

    Update step_1_data_prep.R

--- step_1_data_prep.R
+++ step_1_data_prep.R
@@ -25,7 +25,7 @@ yvars     <- colnames( WAIS_US )
 # Number of observed variables
 ny        <- length( yvars ) 
 
-# covariance matrix used in psychonetrics models
+# covariance matrix to be analyzed
 cov       <- ( n_Hungary - 1 )/n_Hungary*WAIS_Hungary
 
 # latent constructs to be measured (etas)
@@ -33,36 +33,34 @@ lvars     <- c( "P", # Perceptual
                 "V", # Verbal
                 "W", # Working Memory
                 "S"  # Speed 
-              )
+               )
 
 # Number of latent constructs
 ne        <- length( lvars ) 
 
 # theoretical pattern of factor loadings (simple structure)
 lambda_g  <- lambda_b <- matrix( c ( #P  V  W  S  g 
-                         1, 0, 0, 0, 0,# BD
-                         0, 1, 0, 0, 0, # SI
-                         0, 0, 1, 0, 0, # DS
-                         1, 0, 0, 0, 0, # MR
-                         0, 1, 0, 0, 0, # VC
-                         0, 0, 1, 0, 0, # AR 
-                         0, 0, 0, 1, 0, # SS
-                         1, 0, 0, 0, 0, # VP
-                         0, 1, 0, 0, 0, # IN
-                         0, 0, 0, 1, 0, # CD
-                         0, 0, 1, 0, 0, # LN
-                         0, 0, 1, 0, 0, # FW  
-                         0, 1, 0, 0, 0, # CO
-                         0, 0, 0, 1, 0, # CA
-                         1, 0, 0, 0, 0  # PC 
-                       ),
-                     ncol = ne + 1, 
-                     byrow = TRUE )
+                                     1, 0, 0, 0, 0,# BD
+                                     0, 1, 0, 0, 0, # SI
+                                     0, 0, 1, 0, 0, # DS
+                                     1, 0, 0, 0, 0, # MR
+                                     0, 1, 0, 0, 0, # VC
+                                     0, 0, 1, 0, 0, # AR 
+                                     0, 0, 0, 1, 0, # SS
+                                     1, 0, 0, 0, 0, # VP
+                                     0, 1, 0, 0, 0, # IN
+                                     0, 0, 0, 1, 0, # CD
+                                     0, 0, 1, 0, 0, # LN
+                                     0, 0, 1, 0, 0, # FW  
+                                     0, 1, 0, 0, 0, # CO
+                                     0, 0, 0, 1, 0, # CA
+                                     1, 0, 0, 0, 0  # PC 
+                                     ),
+                                 ncol = ne + 1, 
+                                 byrow = TRUE )
 
-# In the bifactor model the subtests measure g directly
-lambda_b[ , ne + 1 ] <- 1
-
-# # factor loadings in the second order (in the g model only)
+# In the higher order factor model, 
+# g is the explanadum of the correlation among the first order factors
 beta_g     <- matrix( c ( #P  V  W  S g  
                            0, 0, 0, 0, 1, # P
                            0, 0, 0, 0, 1, # V
@@ -73,7 +71,15 @@ beta_g     <- matrix( c ( #P  V  W  S g
                      ncol = ne + 1, 
                      byrow = TRUE )
 
-# Exract a network form the US Sample
+# In the bifactor model,
+# the subtests measure g directly (so tests are not unidimensional any longer)
+lambda_b[ , ne + 1 ] <- 1
+
+# In a network model, 
+# it is assumed that the latent factors are not realistic
+# rather the positive manifold is the result of reciprocal interaction
+
+# Let's use a network extracted from the US Sample
 NWModel_US <- ggm( covs = ( n_US - 1 )/n_US*WAIS_US,
                    omega = "Full",
                    nobs = n_US )
@@ -84,7 +90,8 @@ NWModel_US <- NWModel_US %>% prune( alpha = 0.01, recursive = TRUE )
 # Aim for further improvement
 NWModel_US <- NWModel_US %>% stepup 
 
-# Extract the adjacency matrix and use it as confirmatory network in the Hungarian sample
+# Get the adjacency matrix 
+# This the confirmatory network, to be fitted in the Hungarian sample
 omega <- 1*( getmatrix( NWModel_US, "omega" ) !=0 )
 
 
@@ -111,15 +118,18 @@ NWModel   <- ggm( covs = cov,
                   nobs = n_Hungary,
                   omega = omega )
 
-# Put all models in a list
+# Put the models in a list
 models    <- list( HFModel, BFModel, NWModel )
 
-# Run all models on the observed Hungarian WAIS-IV covariance (correlation) matrix 
+# Run the models on the observed Hungarian WAIS-IV covariance (correlation) matrix 
 results   <- lapply( models, function(i) i %>% runmodel ) 
 
-# Extract the model standardized model implied covariance (correlation) matrices
+# Extract the (standardized) model implied covariance matrices
+# i.e, the model implied correlation matrices
 st_sigmas <- lapply( results, 
-                     function(i) { cov2cor ( matrix( getmatrix( i, "sigma"  ),
-                                                     ny, ny, TRUE, 
-                                                     list( yvars, yvars  ) ) ) } )
+                     function( i ) { cov2cor ( matrix( getmatrix( i, "sigma"  ),
+                                                       ny, 
+                                                       ny, 
+                                                       byrow = TRUE, 
+                                                       dimnames = list( yvars, yvars  ) ) ) } )
 

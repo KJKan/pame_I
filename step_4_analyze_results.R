72077f9a74c99eee3c74714acb64bd6b451e2b3b
Kees-Jan Kan <k.j.kan@uva.nl>
2021-02-22 21:58:40 +0100

    Update step_4_analyze_results.R

--- step_4_analyze_results.R
+++ step_4_analyze_results.R
@@ -1,1 +1,24 @@
+# ----- Data analysis
 
+# Clear working memory
+rm(list = ls())
+
+# Load required packages
+library( "psychonetrics" )
+library( "dplyr" )
+source( '0_helperfunctions.R' )
+
+# load list of results 
+load( url( 'https://www.dropbox.com/s/w3yqqxdz54ic3rd/simres.Rdata?raw=TRUE' ) )
+
+# Extract results
+rmseas <- extractFitm( simres, 'rmsea' )
+cfis   <- extractFitm( simres, 'cfi'   )
+nfis   <- extractFitm( simres, 'nfi'   )
+
+# makes plots
+#pdf( 'simplots.pdf' )
+histFitm( rmseas )
+histFitm( cfis )
+histFitm( nfis )
+#dev.off()

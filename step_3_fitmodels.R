4910ba6784b7bde5a417245b95d6fdc1cb861c42
Kees-Jan Kan <k.j.kan@uva.nl>
2021-02-22 21:45:43 +0100

    Update step_3_fitmodels.R

--- step_3_fitmodels.R
+++ step_3_fitmodels.R
@@ -1,1 +1,21 @@
+# ----- Fit models
 
+# Clear working memory
+rm(list = ls())
+
+# Load required packages and functions
+library( "psychonetrics" )
+library( "dplyr" )
+source( '0_helperfunctions.R' )
+
+# load models and simulated data
+load( url( 'https://www.dropbox.com/s/8o197jshr0itki9/models.Rdata?raw=TRUE' ) ) # models
+load( url( 'https://www.dropbox.com/s/hlqu44ltycc3gzt/simdat.Rdata?raw=TRUE' ) ) # simulated data
+
+# fit all models to all datasets
+simres <- lapply( models, 
+                  function( model ) lapply( simdat, 
+                                            function(i) apply( i, 
+                                                               3, 
+                                                               function( dat ) fitModel( dat, 
+                                                                                         model ) ) ) )
